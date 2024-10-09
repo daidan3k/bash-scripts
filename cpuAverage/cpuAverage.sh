a773d2f8e4a403f225a55dc789f0474978b28356
daidan3k <daidan2003@gmail.com>
2024-10-09 20:56:40 +0200

    20:55 - Ara s'ordena per %CPU pero sempre hi ha 1 o 2 errors en algun PID al calcular el %

--- cpuAverage/cpuAverage.sh
+++ cpuAverage/cpuAverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="/home/daidan/scripts/cpuAverage"
+DIR="/home/daidan/bash-scripts/cpuAverage"
 
 if [ $# -ne 0 ]
 then
@@ -12,13 +12,16 @@ mapfile -t PIDS < <(ps -e -o pid=)
 
 declare -A AVERAGES
 
+echo Calculating average CPU consumption of every process...
 for PID in "${PIDS[@]}"
 do
-	AVERAGES[$PID]="$($DIR/cpuPID.sh $PID)"
+	if [ $PID -ne $$ ]
+	then
+		AVERAGES[$PID]="$($DIR/cpuPID.sh $PID)"
+	fi
 done
 
 for PID in "${!AVERAGES[@]}"
 do
-	echo "PID: $PID CPU: ${AVERAGES[$PID]}"
-done | sort -k2 -nr
-
+	echo $PID ' - ' ${AVERAGES["$PID"]}
+done | sort -rn -k3
