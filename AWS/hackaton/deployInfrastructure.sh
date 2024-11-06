f1fd98f9844c756986781f13fa899d180d36cb25
daidan3k <daidan2003@gmail.com>
2024-11-06 20:50:36 +0100

    una mica de gestio de parametres

--- AWS/hackaton/deployInfrastructure.sh
+++ AWS/hackaton/deployInfrastructure.sh
@@ -11,4 +11,51 @@ ARGUMENTS:
 Note: Ensure to follow the specified format for users and passwords.
 """
 
-echo "$USAGE"
+if [ $# -ne 6 ]
+then
+	echo "$USAGE"
+	exit 1
+fi
+
+extract_argument() {
+	echo "${2:-${1#*=}}"
+}
+
+DOMAIN=""
+CLIENTS=""
+declare -A USERS_PASS
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+		-d) # Domain name
+			DOMAIN=$(extract_argument $@)
+			shift
+			;;
+		-c) # Number of clients
+			CLIENTS=$(extract_argument $@)
+			shift
+			if [ $CLIENTS -gt 10 ] && [ $CLIENTS -lt 1 ]
+			then
+				echo "Number of clients must be between 1 and 10"
+				exit 1
+			fi
+			;;
+		-u) # Users and passwords
+			# Separar per parelles d'usuari i contrasenya
+			IFS='/' read -ra pairs <<< "$(extract_argument $@)"
+			# Per cada parella separar-la i guardar en un array associatiu
+			for pair in "${pairs[@]}"; do
+				IFS=',' read -r key value <<< "$pair"
+    				USERS_PASS["$key"]="$value"
+			done
+			;;
+	esac
+	shift
+done
+
+echo $DOMAIN
+echo $CLIENTS
+for key in "${!USERS_PASS[@]}"; do
+    echo "$key -> ${USERS_PASS[$key]}"
+done
